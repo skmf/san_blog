fdbce5248b3f2c932f1cac9ad4310d59eaeba3eb
San Sample Project <skmf201704@gmail.com>
2017-11-17 04:10:18 +0000

    added bootstrap

--- db/migrate/20171116155539_add_description_to_articles_second.rb
+++
@@ -1,8 +0,0 @@
-class AddDescriptionToArticlesSecond < ActiveRecord::Migration
-  def change
-  add_column :articles, :title, :string
-  add_column :articles, :description, :text
-  add_column :articles, :created_at, :datetime
-  add_column :articles, :updated_at, :datetime
-  end
-end
