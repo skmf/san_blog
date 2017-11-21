0239900231af830e3c5b2fa2a26392705cb585bd
San Sample Project <skmf201704@gmail.com>
2017-11-21 02:12:33 +0000

    Added user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
     get 'about', to: 'welcome#about'
     
     resources :articles
-
+    
+    get 'signup', to: 'users#new'
+    # post 'users', to: 'users#create'
+    resources :users, except: [:new]
+    
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
